16eeb3809eaa3d2b308386b133d313b6059cb621
Hit Magrath <hitmagrath@gmail.com>
2025-09-01 14:21:34 +0200

    special message

--- scripts/simplejs1.js
+++ scripts/simplejs1.js
@@ -1,6 +1,10 @@
 const listItems = document.querySelectorAll("li");
-const myHeading = document.querySelector("h2");
+const myHeading1 = document.querySelector("h2");
 const myImg = document.querySelector("img");
+
+let myButton = document.querySelector("button");
+let myMessage = document.getElementById("message");
+
 
 myImg.addEventListener("click",function() {
     const mySrc = myImg.getAttribute("src");
@@ -12,14 +16,25 @@ myImg.addEventListener("click",function() {
     }
 } )
 
-myHeading.addEventListener("click" , function() {
-    const myContent = myHeading.textContent;
+myHeading1.addEventListener("click" , function() {
+    const myContent = myHeading1.textContent;
     if (myContent === "Click me !") {
-        myHeading.textContent= "Hello, world!";
+        myHeading1.textContent= "Hello, world!";
     } else{
-        myHeading.textContent= "Click me !";
+        myHeading1.textContent= "Click me !";
     }
 });
+
+
+function setUserName() {
+    const myName = prompt("please enter your name.");
+    if (!myName){
+        setUserName();
+    }else {
+        localStorage.setItem("name" ,myName);
+        myMessage.textContent = `Java Script is cool, ${myName}`;
+    }
+}
 
 
 function toggleDone(e){
@@ -32,5 +47,9 @@ function toggleDone(e){
 
 listItems.forEach((item)=> {
     item.addEventListener("click",toggleDone);
+});
+
+myButton.addEventListener("click" , () => {
+    setUserName();
 });
 
